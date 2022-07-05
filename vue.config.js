b4d2d35b5cc7e64f5e32d974b83104bfad54943a
zs <2283683403@qq.com>
2022-07-05 10:16:39 +0800

    feat(跨域，axios封装): 跨域，axios封装

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    https: false,
+    host: 'localhost',
+    port: 9999,
+    proxy: {
+      '/dev-api': {
+        target: '',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dev-api': ''
+        }
+      }
+    }
+  }
 })
